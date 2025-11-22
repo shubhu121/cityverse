7c85e83ca1260439f319216ca0a35ea705c24b4a
shubhu121 <shubhucsrldelhi@gmail.com>
2025-11-22 17:24:07 +0530

    gh-build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,5 +8,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.[contenthash].js',
     clean: true
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'css', to: 'css' },
+        { from: 'js', to: 'js' },
+        { from: 'img', to: 'img', noErrorOnMissing: true },
+        { from: 'assets', to: 'assets', noErrorOnMissing: true }
+      ]
+    })
+  ]
 };
